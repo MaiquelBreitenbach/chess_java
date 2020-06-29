efb8f2c39f0e7c0f7779e73e3487bb4d1a296e15
Maiquel Breitenbach <maiquel.breitenbach@gmail.com>
2020-06-28 22:44:30 -0300

    Placing pieces on the board

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -5,9 +5,9 @@ public class Position {
 	private int column;
 	private int row;
 	
-	public Position(int column, int row) {
-		this.column = column;
+	public Position(int row, int column) {
 		this.row = row;
+		this.column = column;
 	}
 
 	public int getColumn() {
